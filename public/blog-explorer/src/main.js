649c699178d60d643ab4b8a3b966b231dc6c12db
Hesham Hassan <hesham.elghndour@gmail.com>
2026-06-19 09:44:44 +0200

    Blog Explorer starter: drop Promise.all from the wiring hint

--- public/blog-explorer/src/main.js
+++ public/blog-explorer/src/main.js
@@ -11,10 +11,8 @@ const main = async () => {
   //  1. createPostDialog(document.querySelector('.dialog'))   // listens for 'post:open'
   //  2. const list = createPostList(document.querySelector('.posts'))
   //  3. createSearchBar(document.querySelector('.search'), { onSearch: (q) => list.filter(q) })
-  //  4. show 'Loading…' in `status`, then:
-  //       const [posts, users] = await Promise.all([getPosts(), getUsers()]);
-  //       list.render(joinAuthors(posts, users));
-  //     handle errors by writing a message into `status`.
+  //  4. show 'Loading…' in `status`, then fetch the posts and the authors and
+  //     list.render(joinAuthors(posts, users)). Handle errors by writing into `status`.
 };
 
 main();
